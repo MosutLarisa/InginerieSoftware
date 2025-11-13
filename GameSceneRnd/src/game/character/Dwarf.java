4ae25f945f57feb4171fa8ed522e1f366592defd
MosutLarisa <146415153+MosutLarisa@users.noreply.github.com>
2025-11-13 22:23:16 +0200

    Game scene rendering

--- GameSceneRnd/src/game/character/Dwarf.java
+++ GameSceneRnd/src/game/character/Dwarf.java
@@ -2,10 +2,11 @@ package game.character;
 
 public class Dwarf extends Character {
     public Dwarf(String name, int x, int y) {
-        super(name, x, y, 80, 1, 1);  // age=80, size=1x1
+        super(name, x, y, 80, 1, 1);  
     }
 
     public String getInfo() {
         return "Dwarf age:" + age + " size:" + width + "x" + height;
     }
+
 }
