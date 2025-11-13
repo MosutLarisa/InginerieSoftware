92555ef0227dcd1975801a8e87eacba747c858a7
MosutLarisa <146415153+MosutLarisa@users.noreply.github.com>
2025-11-13 22:23:57 +0200

    Game scene rendering

--- GameSceneRnd/src/game/character/Human.java
+++ GameSceneRnd/src/game/character/Human.java
@@ -2,10 +2,11 @@ package game.character;
 
 public class Human extends Character {
     public Human(String name, int x, int y) {
-        super(name, x, y, 25, 1, 1);  // age=25, size=1x1
+        super(name, x, y, 25, 1, 1);  
     }
 
     public String getInfo() {
         return "Human age:" + age + " size:" + width + "x" + height;
     }
+
 }
